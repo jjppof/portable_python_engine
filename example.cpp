bdb129294c0f0763f661dd2b165fc0f83db14fc6
Joao Paulo Oliveira Fernandes <jjpp.of@gmail.com>
2019-05-21 09:01:39 -0300

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -6,7 +6,7 @@ int main() {
 	handle.LoadModule("example");
 	handle.LoadFunction("my_function", "example");
 	std::vector<int> test = { 1,2,3 };
-	std::vector<std::vector<int> test2 = { test, test, test };
+	std::vector<std::vector<int>> test2 = { test, test, test };
 	PyObject* p_return = handle.CallFunction("my_function", "example", test2, 3.14, true, "testing");
 	auto obj = PythonEngine::PyType_AsVector<std::string>(p_return);
 	return 0;
