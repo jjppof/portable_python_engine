e47f9beb488834f4cc0e9e919891e78fea00df79
Joao Paulo Oliveira Fernandes <jjpp.of@gmail.com>
2019-07-23 13:29:17 -0300

    Update pythonengine.cpp

--- pythonengine.cpp
+++ pythonengine.cpp
@@ -1,6 +1,5 @@
 #include "engine.h"
-#include <windows.h>
-#include "Shlwapi.h"
+#include <filesystem>
 #include <cstdlib>
 
 PythonEngine::PythonEngine() {
@@ -17,15 +16,26 @@ PythonEngine & PythonEngine::getInstance() {
 }
 
 PythonEngine::PyStatus PythonEngine::Initialize(std::vector<std::pair<std::string, PyObject* (*)(void)>>& modules) {
-	if (PathFileExistsA("C:\\Python36\\python.exe"))
+	if (Py_IsInitialized()) {
+		for (auto& module : modules) {
+			PyImport_AddModule(module.first.c_str());
+			PyObject* pyModule = module.second();
+			PyObject* sys_modules = PyImport_GetModuleDict();
+			PyDict_SetItemString(sys_modules, module.first.c_str(), pyModule);
+			Py_DECREF(pyModule);
+		}
+		return PythonAlreadyInitialized;
+	}
+
+	if (std::filesystem::exists("C:\\Python36\\python.exe"))
 		Py_SetPythonHome(L"C:\\Python36");
-	else if (PathFileExistsA("C:\\Python 3.6\\python.exe"))
+	else if (std::filesystem::exists("C:\\Python 3.6\\python.exe"))
 		Py_SetPythonHome(L"C:\\Python 3.6");
-	else if (PathFileExistsA(std::getenv("PYTHONHOME")))
+	else if (std::filesystem::exists(std::getenv("PYTHONHOME")))
 		Py_SetPythonHome(_wgetenv(L"PYTHONHOME"));
-	else if (PathFileExistsA("C:\\Python3\\python.exe"))
+	else if (std::filesystem::exists("C:\\Python3\\python.exe"))
 		Py_SetPythonHome(L"C:\\Python3");
-	else if (PathFileExistsA("C:\\Python\\python.exe"))
+	else if (std::filesystem::exists("C:\\Python\\python.exe"))
 		Py_SetPythonHome(L"C:\\Python");
 	else
 		return PythonHomeNotFound;
